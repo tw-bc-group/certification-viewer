de4a57ade117b9dcf2b9c0195d16409ae0789bb6
yanqian <qianyan@thoughtworks.com>
2019-07-21 09:24:05 +0800

    feat(api): point to a domain

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const rootUrl = '/photos'
+const rootUrl = 'https://cac.thoughtworks.cn:3000/photos'
 
 const actions = {
   get: (query) => axios.get(`${rootUrl}/${query}`)
